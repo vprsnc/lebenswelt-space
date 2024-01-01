f627f1cd0cd403b35b2117e1a400cc65fd18167a
georgy <georgy@analytics-abc.xyz>
2024-01-01 20:57:19 +0200

    added dispatch logic to rendering

--- src/analytics_abc/core.clj
+++ src/analytics_abc/core.clj
@@ -1,8 +1,21 @@
 (ns analytics-abc.core
   (:require [powerpack.markdown :as md]))
 
+(defn render-frontpage [context page]
+  [:html
+   [:head
+    [:title "The Powerblog"]]
+   [:body
+    (md/render-html (:page/body page))
+    [:h2 "Blog posts"]]])
+
 (defn render-page [context page]
-  (md/render-html (:page/body page)))
+  (cond
+    (= "/" (:page/uri page))
+    (render-frontpage context page)
+
+    :else
+    (md/render-html (:page/body page))))
 
 (def config
   {:site/title "analytics-abc"
