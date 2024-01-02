07e2799b562a6e9417110c6302caa3d8cfe063fd
georgy <georgy@analytics-abc.xyz>
2024-01-02 14:19:06 +0200

    export ns added

---
+++ src/analytics_abc/export.clj
@@ -0,0 +1,8 @@
+(ns analytics-abc.export
+  (:require [analytics-abc.core :as blog]
+            [powerpack.export :as export]))
+
+(defn ^:export export! [& args]
+  (-> blog/config
+      (assoc :site/base-url "https://analytics-abc.xyz"
+             export/export!)))
