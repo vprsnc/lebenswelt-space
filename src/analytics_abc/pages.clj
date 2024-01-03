3f108377e639172396d2a2e84efff3089143ce56
georgy <georgy@analytics-abc.xyz>
2024-01-03 11:19:29 +0200

    move frontpage rendering to a separate file

--- src/analytics_abc/pages.clj
+++ src/analytics_abc/pages.clj
@@ -1,25 +1,8 @@
 (ns analytics-abc.pages
    (:require [powerpack.markdown :as md]
              [analytics-abc.layout :as layout]
+             [analytics-abc.pages.frontpage :as frontpage]
              [datomic.api :as d]))
-
-(defn get-blog-posts [db]
-  (->> (d/q '[:find [?e ...]
-              :where
-              [?e :blog-post/author]]
-            db)
-       (map #(d/entity db %))))
-
-(defn render-frontpage [context page]
-  (let [blog-posts (get-blog-posts (:app/db context))]
-     (layout/layout
-      {:title "analytics-abc"}
-      [:article.prose.dark:prose-invert.mx-auto
-       (md/render-html (:page/body page))
-       [:h2 [:i18n ::blog-posts {:n (count blog-posts)}]]
-       [:ul
-        (for [blog-post (get-blog-posts (:app/db context))]
-          [:li [:a {:href (:page/uri blog-post)} (:page/title blog-post)]])]])))
 
 (defn render-article [context page]
   (layout/layout
@@ -33,6 +16,6 @@
 
 (defn render-page [context page]
   (case (:page/kind page)
-    :page.kind/frontpage (render-frontpage context page)
+    :page.kind/frontpage (frontpage/render-page context page)
     :page.kind/blog-post (render-blog-post context page)
     :page.kind/article (render-article context page)))
---
+++ src/analytics_abc/pages/frontpage.clj
@@ -0,0 +1,23 @@
+(ns analytics-abc.pages.frontpage
+  (:require [datomic.api :as d]
+            [analytics-abc.layout :as layout]
+            [powerpack.markdown :as md]))
+
+(defn get-blog-posts [db]
+  (->> (d/q '[:find [?e ...]
+              :where
+              [?e :blog-post/author]]
+            db)
+       (map #(d/entity db %))))
+
+(defn render-page [context page]
+  (let [blog-posts (get-blog-posts (:app/db context))]
+     (layout/layout
+      {:title "analytics-abc"}
+      [:article.prose.dark:prose-invert.mx-auto
+       (md/render-html (:page/body page))
+       [:h2 [:i18n ::blog-posts {:n (count blog-posts)}]]
+       [:ul
+        (for [blog-post (get-blog-posts (:app/db context))]
+          [:li [:a {:href (:page/uri blog-post)} (:page/title blog-post)]])]])))
+
