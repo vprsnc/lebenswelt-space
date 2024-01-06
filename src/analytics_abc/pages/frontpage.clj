d75e5a0dc8818ccaea036d36e8b584457f4c15f5
georgy <georgy@analytics-abc.xyz>
2024-01-06 17:14:02 +0200

    move posts counter to the "all posts"

--- src/analytics_abc/pages/frontpage.clj
+++ src/analytics_abc/pages/frontpage.clj
@@ -16,9 +16,9 @@
       {:title "analytics-abc"}
       [:article.prose.mx-auto
        (md/render-html (:page/body page))
-       [:h2 [:i18n ::blog-posts {:n (count blog-posts)}]]
+       [:h2 [:i18n ::recent-posts {:n (count blog-posts)}]]
        [:ul
         (for [blog-post (take 5 (get-blog-posts (:app/db context)))]
           [:li [:a {:href (:page/uri blog-post)} (:page/title blog-post)]])
-        [:a {:href "/blog/"} "All posts"]]])))
+        [:a {:href "/blog/"} [:i18n ::blog-posts {:n (count blog-posts)}]]]])))
 
