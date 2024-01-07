09cff449b8a8a9ed2ddda172ec21b2ad406d2176
georgy <georgy@analytics-abc.xyz>
2024-01-07 10:41:49 +0200

    tags to the bottom of the posts

--- src/analytics_abc/pages/article.clj
+++ src/analytics_abc/pages/article.clj
@@ -1,6 +1,7 @@
 (ns analytics-abc.pages.article
   (:require [powerpack.markdown :as md]
-            [analytics-abc.layout :as layout]))
+            [analytics-abc.layout :as layout]
+            [analytics-abc.pages.frontpage :refer [get-tags]]))
 
 (defn render-page [context page]
   (layout/layout
@@ -12,4 +13,8 @@
 ;;      [:li [:small "Author: " (:blog-post/author page)]]
       [:li [:small "Created at: " (:blog-post/date-created page)]]
       [:li [:small "Last updated: " (:blog-post/last-updated page)]]]]
-    (md/render-html (:page/body page))]))
+    (md/render-html (:page/body page))
+    [:h6.mx-auto.text-center "Tags"]
+    [:ul  {:id "tagcloud"}
+       (for [tag (get-tags (:app/db context))]
+         [:li [:a {:href (str "/tag/" tag "/")} tag]])]]))
