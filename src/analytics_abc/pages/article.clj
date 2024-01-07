5e33a46c389fbe67e3535f0b7756169a46410c7c
georgy <georgy@analytics-abc.xyz>
2024-01-07 10:38:17 +0200

    add dates to each post

--- src/analytics_abc/pages/article.clj
+++ src/analytics_abc/pages/article.clj
@@ -7,4 +7,9 @@
    {}
    layout/header
    [:article.prose.mx-auto
+    [:p
+     [:ul {:id "tagcloud"}
+;;      [:li [:small "Author: " (:blog-post/author page)]]
+      [:li [:small "Created at: " (:blog-post/date-created page)]]
+      [:li [:small "Last updated: " (:blog-post/last-updated page)]]]]
     (md/render-html (:page/body page))]))
