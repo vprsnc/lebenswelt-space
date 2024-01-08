fd6148a18b99a32ab520ee009d366f49619cb747
georgy <georgy@analytics-abc.xyz>
2024-01-08 20:35:49 +0200

    remove blogpost description from the listing

--- src/perception_monad/pages/blog_listing.clj
+++ src/perception_monad/pages/blog_listing.clj
@@ -14,6 +14,5 @@
        [:h5
         [:small (:blog-post/date-created blog-post)]
         " - "
-        [:a {:href (:page/uri blog-post)} (:page/title blog-post)]]
-       [:p (:open-graph/description blog-post)]])]]
+        [:a {:href (:page/uri blog-post)} (:page/title blog-post)]]])]]
    ))
--- src/perception_monad/pages/frontpage.clj
+++ src/perception_monad/pages/frontpage.clj
@@ -29,8 +29,7 @@
           [:h5
            [:small (:blog-post/date-created blog-post)]
            " - "
-           [:a {:href (:page/uri blog-post)} (:page/title blog-post)]]
-          [:p (:open-graph/description blog-post)]])
+           [:a {:href (:page/uri blog-post)} (:page/title blog-post)]]])
        [:a {:href "/blog/"} [:i18n ::blog-posts {:n (count blog-posts)}]]]
       [:h3.text-center "Tags"]
       [:ul.tagcloud {:id "tagcloud"}
